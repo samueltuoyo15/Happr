00e44a1ff9b0d932d36b5bf40c1e1a42bd98dc53
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-10-19 18:25:32 +0100

    Remove unused import from app.module.ts
    Eliminated the unused 'throttlers' import from @nestjs/throttler to clean up the module imports.
    
    Co-Authored-By: Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
    Co-Authored-By: Samuel Tuoyo  <samueltuoyo9082@gmail.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { ConfigModule } from "@nestjs/config"
 import { MailModule } from './modules/mail/mail.module';
 import { BullModule } from '@nestjs/bullmq';
 import { UserModule } from './modules/user/user.module';
-import { ThrottlerGuard, ThrottlerModule, throttlers } from "@nestjs/throttler"
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler"
 import { APP_GUARD } from '@nestjs/core';
 
 @Module({
